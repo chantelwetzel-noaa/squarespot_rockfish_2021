4c703d1a8b81269fa9ba700574a4d5cf0aabdf77
Chantel Wetzel <chantel.wetzel@noaa.gov>
2020-12-01 13:02:21 -0800

    fix: update wd

--- write_up/_create_doc.R
+++ write_up/_create_doc.R
@@ -5,7 +5,7 @@ library(sa4ss)
 
 
 # Specify the directory for the document
-setwd("C:/Assessments/2021/squarespot_rockfish/write_up")
+setwd("C:/Assessments/2021/squarespot_rockfish_2021/write_up")
 
 # Create the needed items to generate the "right" template that would be based on the inputs here:
 sa4ss::draft(authors = c("Chantel R. Wetzel", "Brian J. Langseth", "Jason M. Cope", "John E. Budrick"),
@@ -17,7 +17,7 @@ sa4ss::draft(authors = c("Chantel R. Wetzel", "Brian J. Langseth", "Jason M. Cop
   			 edit = FALSE)
 
 # Create a model Rdata object
-read_model(mod_loc = "C:/Assessments/2021/squarespot_rockfish/models/0.01_init_model_updated_catches")
+read_model(mod_loc = "C:/Assessments/2021/squarespot_rockfish_2021/models/0.01_init_model_updated_catches")
 
 if(file.exists("_00.Rmd")){
 	file.remove("_00.Rmd")
