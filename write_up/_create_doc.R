3f9cda85088ed4665c0c9da9cd27daa208700b7f
Chantel Wetzel <chantel.wetzel@noaa.gov>
2021-01-01 08:50:14 -0800

    updated calls

--- write_up/_create_doc.R
+++ write_up/_create_doc.R
@@ -2,6 +2,9 @@
 detach("package:sa4ss", unload = TRUE)
 remotes::install_github("nwfsc-assess/sa4ss")
 library(sa4ss)
+library(r4ss)
+
+devtools::load_all("C:/Users/Chantel.Wetzel/Documents/GitHub/r4ss")
 
 
 # Specify the directory for the document
@@ -17,14 +20,24 @@ sa4ss::draft(authors = c("Chantel R. Wetzel", "Brian J. Langseth", "Jason M. Cop
   			 edit = FALSE)
 
 # Create a model Rdata object
-read_model(mod_loc = "C:/Assessments/2021/squarespot_rockfish_2021/models/0.01_init_model_updated_catches")
+# Create a model Rdata object
+sa4ss::read_model(
+          		  mod_loc = "C:/Assessments/2021/squarespot_rockfish_2021/models/2.3_hessian",
+				  create_plots = FALSE, 
+				  save_loc = file.path(getwd(), "tex_tables"),
+				  verbose = TRUE)
+es_table_tex(dir = file.path(getwd(), 'tables'), 
+			 save_loc = file.path(getwd(), 'tex_tables'),
+			 csv_name = "all_tables.csv")
+
 
 if(file.exists("_main.Rmd")){
 	file.remove("_main.Rmd")
 }
-
 # Render the pdf
-bookdown::render_book("00a.Rmd", clean = FALSE)
+bookdown::render_book("00a.Rmd", clean=FALSE, output_dir = getwd())
+
+
 
 
 # Use to only render a specific section which can be quicker
