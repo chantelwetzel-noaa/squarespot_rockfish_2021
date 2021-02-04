079b417d283d64ad729755ae2ad9114a73529af4
Chantel Wetzel <chantel.wetzel@noaa.gov>
2021-02-04 09:18:51 -0800

    clean up script and add comments

--- write_up/_create_doc.R
+++ write_up/_create_doc.R
@@ -1,42 +1,61 @@
 
-detach("package:sa4ss", unload = TRUE)
-remotes::install_github("nwfsc-assess/sa4ss")
+#detach("package:sa4ss", unload = TRUE)
+#remotes::install_github("nwfsc-assess/sa4ss")
+#devtools::load_all("C:/Users/Chantel.Wetzel/Documents/GitHub/r4ss")
 library(sa4ss)
 library(r4ss)
-
-devtools::load_all("C:/Users/Chantel.Wetzel/Documents/GitHub/r4ss")
-
 
 # Specify the directory for the document
 setwd("C:/Assessments/2021/squarespot_rockfish_2021/write_up")
 
 # Create the needed items to generate the "right" template that would be based on the inputs here:
-sa4ss::draft(authors = c("Chantel R. Wetzel", "Brian J. Langseth", "Jason M. Cope", "John E. Budrick"),
-  			 species = "Squarespot Rockfish",
-  			 latin = "Sebastes hopkinsi",
-  			 coast = "US West - California",
-  			 type = c("sa"),
-  			 create_dir = FALSE,
-  			 edit = FALSE)
+# sa4ss::draft(authors = c("Chantel R. Wetzel", "Brian J. Langseth", "Jason M. Cope", "John E. Budrick"),
+#   			 species = "Squarespot Rockfish",
+#   			 latin = "Sebastes hopkinsi",
+#   			 coast = "US West - California",
+#   			 type = c("sa"),
+#   			 create_dir = FALSE,
+#   			 edit = FALSE)
 
 # Create a model Rdata object
 # Create a model Rdata object
-mod_loc = "C:/Assessments/2021/squarespot_rockfish_2021/models/2.5_rec_devs_sigmaR60"
+# mod_loc = "C:/Assessments/2021/squarespot_rockfish_2021/models/2.5_rec_devs_sigmaR60"
+
+###########################################################################
+# Create object based on a new model
+###########################################################################
+
+base_name = "2.5_rec_devs_sigmaR60"
+mod_loc = file.path(
+          "//nwcfile/FRAM/Assessments/CurrentAssessments/DataModerate_2021/Squarespot_Rockfish/models",
+          base_name)
+
 sa4ss::read_model(
           mod_loc = mod_loc,
-				  create_plots = TRUE, 
+				  create_plots = FALSE, 
+          html = FALSE,
 				  save_loc = file.path(getwd(), "tex_tables"),
 				  verbose = TRUE)
-source("C:/Users/Chantel.Wetzel/Documents/GitHub/sa4ss/R/es_table_tex.R")
 
+###########################################################################
+# Create tex files from the executive summary tables
+###########################################################################
+#source("C:/Users/Chantel.Wetzel/Documents/GitHub/sa4ss/R/es_table_tex.R")
 sa4ss::es_table_tex(dir = mod_loc, 
             save_loc = file.path(getwd(), "tex_tables"), 
             csv_name = "table_labels.csv")
 
+###########################################################################
 # Read and create tex files for tables listed in "table" folder in the doc
+###########################################################################
+
 sa4ss::es_table_tex(dir = file.path(getwd(), 'tables'), 
             save_loc = file.path(getwd(), "tex_tables"), 
             csv_name = "all_tables.csv")
+
+###########################################################################
+# Render Call 
+###########################################################################
 
 if(file.exists("_main.Rmd")){
 	file.remove("_main.Rmd")
@@ -44,8 +63,5 @@ if(file.exists("_main.Rmd")){
 # Render the pdf
 bookdown::render_book("00a.Rmd", clean=FALSE, output_dir = getwd())
 
+#########################################################################
 
-
-
-# Use to only render a specific section which can be quicker
-bookdown::preview_chapter("01executive.Rmd", preview = TRUE, clean = FALSE)
